e632e49d84034f1da5df69bc1717f6f65e44b33f
otnansirk <iam.otnansirk@gmail.com>
2026-09-24 11:40:42 +0700

    fix: success page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 
+import { resolve } from 'path';
+
 export default defineConfig({
   server: {
     allowedHosts: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        success: resolve(__dirname, 'success.html')
+      }
+    }
   }
 });
